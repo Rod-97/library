46267930d7c28fbc9ed2b54686be7a5f6eb4db43
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-04-09 17:16:38 +0200

    book constructor

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test");
+const myLibrary = [];
+
+function Book(title, author, year) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+function addBookToLibrary(title, author, year) {
+  const book = new Book(title, author, year);
+  myLibrary.push(book);
+}
