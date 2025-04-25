914d9b7ef689329186455c27d4288bb40a89abfc
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-04-25 14:16:17 +0200

    convert functions to class

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const myLibrary = [];
 
-function Book(title, author, year, imageUrl) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.year = year;
-  this.imageUrl = imageUrl;
-  this.read = false;
-}
+class Book {
+  constructor(title, author, year, imageUrl) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.imageUrl = imageUrl;
+    this.read = false;
+  }
 
-function addBookToLibrary(title, author, year, imageUrl) {
-  const book = new Book(title, author, year, imageUrl);
-  myLibrary.push(book);
-}
+  addToLibrary() {
+    myLibrary.push(this);
+  }
 
-function displayLibrary() {
-  const main = document.getElementsByClassName("main")[0];
-
-  myLibrary.forEach((book) => {
+  display() {
+    const main = document.getElementsByClassName("main")[0];
     const card = document.createElement("div");
     card.classList.add("card");
-    card.id = book.id;
+    card.id = this.id;
 
     const title = document.createElement("h2");
     const img = document.createElement("img");
@@ -37,13 +35,13 @@ function displayLibrary() {
     read.classList.add("read");
     deleteButton.classList.add("delete-btn");
 
-    title.textContent = book.title;
-    img.src = book.imageUrl;
+    title.textContent = this.title;
+    img.src = this.imageUrl;
 
     const authorSpan = document.createElement("span");
     const authorName = document.createElement("span");
     authorSpan.textContent = "Author: ";
-    authorName.textContent = book.author;
+    authorName.textContent = this.author;
     authorSpan.classList.add("info-span");
     authorName.classList.add("author-name");
     author.appendChild(authorSpan);
@@ -52,7 +50,7 @@ function displayLibrary() {
     const yearSpan = document.createElement("span");
     const yearContent = document.createElement("span");
     yearSpan.textContent = "Year: ";
-    yearContent.textContent = book.year;
+    yearContent.textContent = this.year;
     yearSpan.classList.add("info-span");
     yearContent.classList.add("year");
     year.appendChild(yearSpan);
@@ -70,47 +68,54 @@ function displayLibrary() {
     card.appendChild(container);
     card.appendChild(deleteButton);
     main.appendChild(card);
-  });
+  }
+
+  static markAsRead(bookId) {
+    const book = myLibrary.find((book) => book.id === bookId);
+    book.read ? null : (book.read = true);
+  }
+
+  static delete(bookId) {
+    const bookIdx = myLibrary.findIndex((book) => {
+      return book.id === bookId;
+    });
+    myLibrary.splice(bookIdx, 1);
+  }
 }
 
-function markAsRead(bookId) {
-  const book = myLibrary.find((book) => book.id === bookId);
-  book.read ? null : (book.read = true);
-}
-
-function deleteBook(bookId) {
-  const bookIdx = myLibrary.findIndex((book) => {
-    return book.id === bookId;
-  });
-  myLibrary.splice(bookIdx, 1);
-}
-
-addBookToLibrary(
+const book1 = new Book(
   "The Beginning of Infinity",
   "David Deutsch",
   2011,
   "https://m.media-amazon.com/images/I/716R8HCtH+L._AC_UF1000,1000_QL80_.jpg"
 );
-addBookToLibrary(
+book1.addToLibrary();
+
+const book2 = new Book(
   "Atlas Shrugged",
   "Ayn Rand",
   1957,
   "https://m.media-amazon.com/images/I/41pTUDMiQNL._AC_UF1000,1000_QL80_.jpg"
 );
-addBookToLibrary(
+book2.addToLibrary();
+
+const book3 = new Book(
   "Twenty Thousand Leagues Under the Sea",
   "Jules Verne",
   1870,
   "https://m.media-amazon.com/images/I/81nNl5oyfXL._AC_UF1000,1000_QL80_.jpg"
 );
-addBookToLibrary(
+book3.addToLibrary();
+
+const book4 = new Book(
   "The Origin of Species",
   "Charles Darwin",
   1859,
   "https://m.media-amazon.com/images/I/71ea0N8I14L.jpg"
 );
+book4.addToLibrary();
 
-displayLibrary();
+myLibrary.forEach((book) => book.display());
 
 const newBookBtn = document.querySelectorAll(".new-book-btn")[0];
 const dialog = document.querySelectorAll(".dialog")[0];
@@ -130,10 +135,11 @@ submitBtn.addEventListener("click", (e) => {
   const author = document.getElementById("author").value;
   const year = document.getElementById("year").value;
   const imgUrl = document.getElementById("img-url").value;
-  addBookToLibrary(title, author, year, imgUrl);
+  const newBook = new Book(title, author, year, imgUrl);
+  newBook.addToLibrary();
+  newBook.display();
   dialog.classList.remove("active");
   shadow.classList.remove("active");
-  displayLibrary();
 });
 
 shadow.addEventListener("click", () => {
@@ -144,7 +150,7 @@ shadow.addEventListener("click", () => {
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener("click", () => {
     const card = checkbox.parentElement.parentElement.parentElement;
-    markAsRead(card.id);
+    Book.markAsRead(card.id);
   });
 });
 
@@ -155,7 +161,7 @@ deleteButtons.forEach((deleteBtn) => {
     );
     if (userInput.toLowerCase() !== "yes") return;
     const card = deleteBtn.parentElement;
-    deleteBook(card.id);
+    Book.delete(card.id);
     card.remove();
   });
 });
